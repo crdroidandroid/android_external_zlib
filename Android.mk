5c1b3f70c0bf5497d3b639d1342cba086a85ee81
Alex Naidis <alex.naidis@linux.com>
2016-11-21 20:26:18 +0100

    zlib: ensure to unset local variable
    zlib_files has to be unset.
    
    Change-Id: Iaad2b00a65a1ca6f3d9bddc2a15de3bb23c5e354
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- Android.mk
+++ Android.mk
@@ -97,3 +97,6 @@ include $(BUILD_HOST_EXECUTABLE)
 $(TARGET_OUT_COMMON_GEN)/zlib_fingerprint : $(wildcard $(LOCAL_PATH)/src/*.[ch])
 	printf '%s\n' $^ | LC_ALL=C sort | xargs cat | shasum -a 256 | \
 		awk '{printf $$1}' > $@
+
+# Unset local variable
+zlib_files:=
