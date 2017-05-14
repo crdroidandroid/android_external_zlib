867ab33ec4392889f047027c69818e4fa04e0bec
Park Ju Hyung <qkrwngud825@gmail.com>
2017-05-15 03:44:27 +0900

    zlib: enable unaligned memory access
    arm32, arm64, x86 and x86_64 are all capable of
    handling unaligned memory access.
    
    Enable UNALIGNED_OK in order to improve performance.
    
    Test: used zpipe.c on the host, arm32 and arm64 devices
    Change-Id: I577c82165a36d2702802f8cd5ee526c8aa6d098d
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- Android.mk
+++ Android.mk
@@ -24,7 +24,7 @@ zlib_files := \
 
 LOCAL_MODULE := libz
 LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS += -O3 -DUSE_MMAP -DZLIB_CONST
+LOCAL_CFLAGS += -O3 -DUSE_MMAP -DZLIB_CONST -DUNALIGNED_OK
 
 # TODO: This is to work around b/24465209. Remove after root cause is fixed
 LOCAL_LDFLAGS_arm := -Wl,--hash-style=both
