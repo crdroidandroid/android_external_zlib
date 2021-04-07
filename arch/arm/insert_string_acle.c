dddc74d0c624dff3afddd9988ca60e41cc0923c0
Danny Lin <danny@kdrag0n.dev>
2021-04-06 21:39:07 -0700

    Port zlib-ng to the Android platform
    - Add Soong blueprint
    - Add CRC32 fixes
    - Add NDK mappings
    
    All changes were based on the Android fork of Chromium zlib.
    
    As reported by simpleperf, a considerable amount of CPU time is spent
    decompressing Android binary XML resources when opening and closing
    activities in Settings. Because of this, some binary XMLs from
    framework-res.apk have been used for zlib_bench testing.
    
    zlib-ng is significantly faster than vanilla zlib and Chromium zlib
    (with optimizations enabled) in terms of compression. 64-bit benchmarks
    with zlib_bench64 on Android:
    
    Compression:
    +------------------------------------------+------+---------------+---------+-------+
    |                   File                   | zlib | Chromium zlib | zlib-ng |  Unit |
    +------------------------------------------+------+---------------+---------+-------+
    |     res/anim/activity_open_enter.xml     | 10.4 |       16      |   25.1  | MiB/s |
    |    res/anim/activity_close_enter.xml     | 9.3  |      15.5     |   21.1  | MiB/s |
    |         res/transition/move.xml          | 9.7  |      16.4     |   21.6  | MiB/s |
    | res/drawable/stat_sys_battery_charge.xml | 13.3 |      22.4     |   33.2  | MiB/s |
    +------------------------------------------+------+---------------+---------+-------+
    
    The gains are less significant for decompression, and Chromium zlib is
    generally slighty faster in this case. However, zlib-ng still tends to
    be faster than vanilla zlib:
    
    Decompression:
    +------------------------------------------+-------+---------------+---------+-------+
    |                   File                   |  zlib | Chromium zlib | zlib-ng |  Unit |
    +------------------------------------------+-------+---------------+---------+-------+
    |     res/anim/activity_open_enter.xml     | 136.8 |     150.6     |  153.2  | MiB/s |
    |    res/anim/activity_close_enter.xml     | 125.7 |     133.5     |   123   | MiB/s |
    |         res/transition/move.xml          | 120.6 |     129.9     |  124.4  | MiB/s |
    | res/drawable/stat_sys_battery_charge.xml | 194.6 |     222.1     |  217.3  | MiB/s |
    +------------------------------------------+-------+---------------+---------+-------+
    
    In combination with enabling -O3 for libpng, zlib-ng speeds up
    screenshot saving by 16% on redfin (Cortex-A55 + Cortex-A76) as compared
    to vanilla zlib.
    
    While Android is likely to be decompressing much more data than it
    compresses, zlib-ng's significantly faster compression should be a
    worthwhile tradeoff for its deficiencies in decompression speed.
    
    zlib versions used for benchmarks:
    Vanilla zlib: [1] (Chromium zlib with optimizations disabled)
    Chromium zlib: [2] with all optimizations enabled
    zlib-ng: [3] with Android porting changes
    All zlib variants were built with -O3, as per commit [4].
    
    [1] https://android.googlesource.com/platform/external/zlib/+/refs/tags/android-11.0.0_r35
    [2] https://android.googlesource.com/platform/external/zlib/+/e620e8065d17bab7c9eefe3a03d7f8b80ca450d6
    [3] https://github.com/zlib-ng/zlib-ng/commit/9f784908599006e9f09599b85b495bdcda1f25c4
    [4] https://android.googlesource.com/platform/external/zlib/+/18e812de

--- arch/arm/insert_string_acle.c
+++ arch/arm/insert_string_acle.c
@@ -12,6 +12,44 @@
 #include "../../zbuild.h"
 #include "../../deflate.h"
 
+#if defined(__clang__)
+/* CRC32 intrinsics are #ifdef'ed out of arm_acle.h unless we build with an
+ * armv8 target, which is incompatible with ThinLTO optimizations on Android.
+ * (Namely, mixing and matching different module-level targets makes ThinLTO
+ * warn, and Android defaults to armv7-a. This restriction does not apply to
+ * function-level `target`s, however.)
+ *
+ * Since we only need four crc intrinsics, and since clang's implementation of
+ * those are just wrappers around compiler builtins, it's simplest to #define
+ * those builtins directly. If this #define list grows too much (or we depend on
+ * an intrinsic that isn't a trivial wrapper), we may have to find a better way
+ * to go about this.
+ *
+ * NOTE: clang currently complains that "'+soft-float-abi' is not a recognized
+ * feature for this target (ignoring feature)." This appears to be a harmless
+ * bug in clang.
+ */
+#define __crc32b __builtin_arm_crc32b
+#define __crc32d __builtin_arm_crc32d
+#define __crc32w __builtin_arm_crc32w
+#define __crc32cw __builtin_arm_crc32cw
+
+#if defined(__aarch64__)
+#define TARGET_ARMV8_WITH_CRC __attribute__((target("crc")))
+#else  // !defined(__aarch64__)
+#define TARGET_ARMV8_WITH_CRC __attribute__((target("armv8-a,crc")))
+#endif  // defined(__aarch64__)
+
+#elif defined(__GNUC__)
+/* For GCC, we are setting CRC extensions at module level, so ThinLTO is not
+ * allowed. We can just include arm_acle.h.
+ */
+#include <arm_acle.h>
+#define TARGET_ARMV8_WITH_CRC
+#else  // !defined(__GNUC__) && !defined(_aarch64__)
+#error ARM CRC32 SIMD extensions only supported for Clang and GCC
+#endif
+
 #define HASH_CALC(s, h, val) \
     h = __crc32w(0, val)
 
