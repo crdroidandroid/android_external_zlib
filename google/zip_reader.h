e52bbfa9e4ef0c9733e56b9b740affb83f55f011
Andrzej Perczak <linux@andrzejperczak.com>
2022-05-25 23:06:25 +0200

    zlib: Merge chromium zlib master
    Commit: 0e6b3ca1fa2963e2c4f145287ce4af5cbb4ebdeb
    Repository: https://chromium.googlesource.com/chromium/src/third_party/zlib
    
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>

--- google/zip_reader.h
+++ google/zip_reader.h
@@ -94,9 +94,14 @@ class ZipReader {
     // if it wants to interpret this path correctly.
     std::string path_in_original_encoding;
 
-    // Path of the entry, converted to Unicode. This path is usually relative
-    // (eg "foo/bar.txt"), but it can also be absolute (eg "/foo/bar.txt") or
-    // parent-relative (eg "../foo/bar.txt"). See also |is_unsafe|.
+    // Path of the entry, converted to Unicode. This path is relative (eg
+    // "foo/bar.txt"). Absolute paths (eg "/foo/bar.txt") or paths containing
+    // ".." or "." components (eg "../foo/bar.txt") are converted to safe
+    // relative paths. Eg:
+    // (In ZIP) -> (Entry.path)
+    // /foo/bar -> ROOT/foo/bar
+    // ../a     -> UP/a
+    // ./a      -> DOT/a
     base::FilePath path;
 
     // Size of the original uncompressed file, or 0 if the entry is a directory.
@@ -122,8 +127,8 @@ class ZipReader {
     // False if the entry is a file.
     bool is_directory;
 
-    // True if the entry path is considered unsafe, ie if it is absolute or if
-    // it contains "..".
+    // True if the entry path cannot be converted to a safe relative path. This
+    // happens if a file entry (not a directory) has a filename "." or "..".
     bool is_unsafe;
 
     // True if the file content is encrypted.
@@ -257,6 +262,10 @@ class ZipReader {
   // reset automatically as needed.
   bool OpenEntry();
 
+  // Normalizes the given path passed as UTF-16 string piece. Sets entry_.path,
+  // entry_.is_directory and entry_.is_unsafe.
+  void Normalize(base::StringPiece16 in);
+
   // Extracts a chunk of the file to the target.  Will post a task for the next
   // chunk and success/failure/progress callbacks as necessary.
   void ExtractChunk(base::File target_file,
@@ -277,8 +286,8 @@ class ZipReader {
   base::WeakPtrFactory<ZipReader> weak_ptr_factory_{this};
 };
 
-// A writer delegate that writes to a given File. This file is expected to be
-// initially empty.
+// A writer delegate that writes to a given File. It is recommended that this
+// file be initially empty.
 class FileWriterDelegate : public WriterDelegate {
  public:
   // Constructs a FileWriterDelegate that manipulates |file|. The delegate will
